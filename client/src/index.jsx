c238ea365fd381e2e7a07609d37e5ef2493da305
Louis <louisla1992@gmail.com>
2021-07-29 19:56:08 -0700

    added to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Reviews from './components/ratingsReviews/ratings.jsx';
+import ProductOverview from './components/productDetails/products.jsx'
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId: '',
+      productId: 28212,
       user: '',
       stars: '',
       reportedArray: [],
@@ -14,9 +15,10 @@ class App extends React.Component {
   render() {
     return (
       <div>Hello World
+        <ProductOverview productId={this.state.productId}/>
         <Reviews props={this.state}/>
       </div>
-    
+
     );
   }
 }
