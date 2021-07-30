466c00edccc42f2a3da261e04e9e0dbdd333b216
isaack87 <ijkim@asu.edu>
2021-07-29 20:06:07 -0700

    Isaac index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Reviews from './components/ratingsReviews/ratings.jsx';
+import SearchBar from '/Users/isaackim/Desktop/AtelierProject/Atelier/client/src/components/questionsAnswers/searchBar.jsx';
+import QuestionsAnswersState from './components/questionsAnswers/QuestionAnswerState.jsx';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,10 +17,21 @@ class App extends React.Component {
   render() {
     return (
       <div>Hello World
+        <QuestionsAnswersState
+        mainProductId={this.state.productId}
+        />
         <Reviews props={this.state}/>
       </div>
-    
+
     );
   }
 }
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+
+
+
+
+
