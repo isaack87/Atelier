5e4a06b70f6b9609203db68a4f769f2684fa2369
isaack87 <ijkim@asu.edu>
2021-07-29 19:09:46 -0700

    searchbar added

---
+++ client/src/components/questionsAnswers/searchbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+    this.search = this.search.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      searchTerm: e.target.value,
+    });
+  }
+
+  search(e) {
+    e.preventDefault();
+    const { searchTerm } = this.state;
+    this.props.onSearch(searchTerm);
+  }
+
+  render() {
+    return (
+      <div className="searchbar">
+        <form>
+          <p className="searchbartitle">QUESTIONS & ANSWERS</p>
+          <br />
+          <input
+            onChange={this.onChange}
+            className="searchfield"
+            placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS ..."
+          />
+          <button type="submit" className="searchbutton" onClick={this.search}>
+            <img className="imgmag" src="search.png" alt="searchlogo" />
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
