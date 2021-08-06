a01c15b16dbe4740cc21c5e3a12ee0c996c31bd3
Helena Tanubrata <helen.tanubrata@sfcm.edu>
2021-08-05 19:47:01 -0700

    working on images modal

--- client/src/components/ratingsReviews/images.jsx
+++ client/src/components/ratingsReviews/images.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import jquery from 'jquery';
+window.$ = window.jquery = jquery;
 
 class Images extends React.Component {
     constructor(props) {
@@ -7,12 +9,47 @@ class Images extends React.Component {
             images: []
         }
          this.createImageDiv = this.createImageDiv.bind(this);
+         this.modalClick = this.modalClick.bind(this);
+    }
+    modalClick = (imageID, imageSrc)=> {
+            //test
+            // $("#app").on("click", ()=> {
+            //     console.log('apop is click')
+            // })
+
+
+        let modal = document.getElementById('mymodal' + imageID)
+     
+        let img = document.getElementsByClassName(imageID);
+        let modalImg = document.getElementById('modalImg' + imageID)
+        console.log('img', img, "modalimg", modalImg);
+
+             modalImg.src = imageSrc;
+             modal.style.display = "block"
+
+        let span = document.getElementsByClassName('close-modal');
+        span.onclick = () => {
+            modal.style.display = 'none';
+        }
+
     }
     createImageDiv(imageArr) {
         let imagesDiv = [];
         imageArr.forEach(image => {
-            imagesDiv.push(<img id= "review-image" key= {image.id} src= {image.url}/>)
-        })
+            imagesDiv.push(
+            <div>
+                <img id= "review-image" className = {image.id} onClick = { (e)=> {
+                    e.preventDefault();
+                    this.modalClick(image.id, image.url);
+                   
+                }} key= {image.id} src= {image.url}/>
+
+                <div id= {"mymodal" + image.id} className = 'modal'>
+                    {/* <span className = 'close-modal'>x</span> */}
+                    <img className="modal-content" id= {"modalImg" + image.id}/>
+                </div>
+            </div>)
+        });
         let div = <div >{imagesDiv}</div>
         this.setState({images: div});
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Reviews from './components/ratingsReviews/ratings.jsx';
 import SearchBar from './components/questionsAnswers/searchBar.jsx';
-import QuestionsAnswersState from './components/questionsAnswers/QuestionAnswerState.jsx';
-import ProductOverview from './components/productDetails/OverviewMain.jsx'
+// import QuestionsAnswersState from './components/questionsAnswers/QuestionAnswerState.jsx';
+// import ProductOverview from './components/productDetails/OverviewMain.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -22,14 +22,7 @@ class App extends React.Component {
 
 
       <div>
-        <ProductOverview productId={this.state.productId} />
-        <div className="q-a-box">
-          <div className="qacontainer">
-        <QuestionsAnswersState
-          productId={this.state.productId}
-        />
-        </div>
-        </div>
+
         <Reviews props={this.state} />
       </div>
 
