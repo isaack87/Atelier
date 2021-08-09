294a79aa3a7b84b009ee16e45bc3cf9ed94561ab
Helena Tanubrata <helen.tanubrata@sfcm.edu>
2021-08-08 20:52:56 -0700

    Working sort options

--- server/RatingsReviewsAPI.js
+++ server/RatingsReviewsAPI.js
@@ -12,11 +12,55 @@ const getReviewsAPI = (productID, sortKind = 'helpful') => {
         url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews?product_id=${productID}&sort=${sortKind}$count=100`,
         headers: { Authorization: config.gitToken },
       };
-      return  axios(options)
+      return axios(options)
       .then(response => {
-          //console.log('response.', response.data)
-          return response.data;
-      })
+          let sortedArr;
+        if (sortKind === 'helpful') {
+            sortedArr = response.data.results.sort((a,b) => {
+                return a.helpfulness > b.helpfulness;
+            });
+         } else if (sortKind === 'newest') {
+            sortedArr = response.data.results.sort((a,b)=> {
+                return new Date(b.date).getTime() - new Date(a.date).getTime();
+            })
+        } else {
+            //first sort by date
+            let sortOne = response.data.results.sort((a,b)=> {
+                return new Date(b.date).getTime() - new Date(a.date).getTime(); 
+            });
+            let length = sortOne.length;
+            //now we run our relevance sort for half the length of the sortOne array. that will push up the helpful reviews, but not all the way up
+            for (let i = 0; i < (Math.floor(sortOne.length / 2)); i++) {
+                for (let a = 0; a < length; a+=2) {
+                    if (sortOne[a + 1].helpfulness > sortOne[a].helpfulness) {
+                        let one = sortOne[a + 1];
+                        let two = sortOne[a];
+                        sortOne[a] = one;
+                        sortOne[a + 1] = two;
+                    }
+                }
+            }
+            sortedArr = sortOne;
+        }
+
+         return sortedArr;
+
+      });
+
+}
+const relevanceSort = (array) => {
+    console.log('sorting in relevance?')
+                
+    for (let i = 0; i < array.length; i+2) {
+        if (array[i + 1].helpfulness > array[i].helpfulness) {
+            let one = array[i + 1];
+            let two = array[i];
+            array[i] = one;
+            array[i + 1] = two;
+        }
+
+    }
+    return array;
 
 }
 
