4856ed13832eab7360d5ce529c9c18a2a83d8771
isaack87 <ijkim@asu.edu>
2021-08-11 00:40:33 -0700

    css search fix

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId: 28212,
+      productId: 28299,
       user: '',
       stars: '',
       reportedArray: [],
