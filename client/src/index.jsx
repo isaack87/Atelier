1cb01626aa5b58c012d6b08f028b48fa5aeff450
Helena Tanubrata <helen.tanubrata@sfcm.edu>
2021-08-12 19:01:19 -0700

    fixed the error by removing a -1

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId: 28299,
+      productId: 28212,
       user: '',
       stars: '',
       reportedArray: [],
